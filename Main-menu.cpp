b13880ca611c48a283365c867ac4d30669aa2b3e
mattiamal <102604998+mattiamal@users.noreply.github.com>
2022-08-02 12:53:46 +0200

    Update Main-menu.cpp

--- Main-menu.cpp
+++ Main-menu.cpp
@@ -23,20 +23,20 @@ int main (){
     wrefresh(win);
     keypad(win, TRUE);
 
-    menu1 *home=new menu1(1, win);
+    menu1 *home=new menu1(1, win);          //creo le finestre come puntatori per poterle eliminare a piacimento
     menu2 *opzioni=new menu2(1, win);
-    while(!home->return_exit() && !home->return_newgame()){
+    while(!home->return_exit() && !home->return_newgame()){         //finche' non creo la partita o non esco posso accedere ai due menu'
         home->titolo();
         home->disegna1();
         home->muovi1(win);
-        if(home->return_opzioni()){
-            werase(win);
+        if(home->return_opzioni()){             //se si attiva il booleano menu allora si pulisce lo schermo e si crea la seconda finestra, cio' avviene finche' 
+            werase(win);                        //il booleano e' vero
             wrefresh(win);
             box (win, 0, 0);
             opzioni->disegna2();
             opzioni->muovi2();
         }
-        if(!opzioni->return_opzioni()){
+        if(!opzioni->return_opzioni()){         //se diventa falso faccio lo stesso
             werase(win);
             wrefresh(win);
             box (win, 0, 0);
@@ -44,7 +44,7 @@ int main (){
             home->muovi1(win);
         }
     }
-    if(home->return_newgame()){
+    if(home->return_newgame()){         //se ho avviato una nuova partita si eliminano i menu' e si crea la finestra di gioco (da collegare tutto con la mappa poi)
         delete opzioni;
         delete home;
         clear();
@@ -53,7 +53,7 @@ int main (){
         box (window, 0, 0);
         wrefresh(window);
     }
-    if(home->return_exit()){
+    if(home->return_exit()){            //se esco dal gioco chiudo tutto e faccio l'endwin
         delete opzioni;
         delete home;
         clear();
