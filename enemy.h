181799bb7a161e33f612843d7e02c3886fecf303
KyoZD <102605858+DhahriAhmed@users.noreply.github.com>
2022-10-10 15:20:11 +0200

    Add files via upload

--- enemy.h
+++ enemy.h
@@ -1,5 +1,6 @@
 #ifndef _ENEMY_H_
 #define _ENEMY_H_
+#include <ncurses.h>
 class enemy
 {
 	public :
@@ -9,27 +10,34 @@ class enemy
 		void mvdown();
 		void mvleft();
 		void mvright();
-		void move(int& j);
+		
 		void display();
 		int fyloc();
+		int fxloc();
 		int getenemx();
 		int getenemy();
 		void undisplay();
+		void verticalmove();
+		void horizontalmove();
+		void diagonal1move();
+		void diagonal2move();
+		//void declife();
 
-	private :
-		int xloc, yloc, xMax, yMax;
+	protected :
+		int xloc, yloc, xMax, yMax, j ;
 		char character;
 		WINDOW *curwin;
 };
 
-enemy::enemy(WINDOW *win, int y, int x , char c)
+enemy::enemy(WINDOW *win, int y, int x , char c )
 {
 	curwin = win;
 	yloc = y;
 	xloc = x;
 	getmaxyx(curwin, yMax, xMax);
-
+	j=0;
 	character = c;
+	//vita = life;
 
 }
 	void enemy::mvup()
@@ -43,12 +51,36 @@ enemy::enemy(WINDOW *win, int y, int x , char c)
 	napms(100);
 	yloc++;
 	}
+
+
+
+void enemy::mvright()
+	{ mvwaddch(curwin, yloc, xloc, ' ');
+	napms(100);
+	xloc++;
+
+	}
+void enemy::mvleft()
+	{ mvwaddch(curwin, yloc, xloc, ' ');
+	napms(100);
+	xloc--;
+
+	}
+
+
+
+
 	void enemy::display(){mvwaddch(curwin, yloc, xloc, character);  }
 	int enemy::fyloc(){int loc=yloc ; return(loc);}
-	void enemy::move(int& j){ 
+	int enemy::fxloc(){int loc= xloc; return(loc);}
 
-		if(fyloc()==1){j=0;}
-		if(fyloc()==yMax-2){j=1;}
+
+
+
+	void enemy::verticalmove(){ 
+
+		if(mvwinch(curwin, yloc+1, xloc)!=32){j=1;}
+		if(mvwinch(curwin, yloc-1, xloc)!=32){j=0;}
 		if(j==0){
 		mvdown();
 		display();
@@ -61,11 +93,279 @@ enemy::enemy(WINDOW *win, int y, int x , char c)
 
 		}
 
+	void enemy::horizontalmove(){ 
+
+		if(mvwinch(curwin, yloc, xloc+1)!=32){j=1;}
+		if(mvwinch(curwin, yloc, xloc-1)!=32){j=0;}
+		if(j==0){
+		mvright();
+		display();
+		}
+		if(j==1){
+		mvleft();
+		display();
+
+		}
+
+		}
+
+
+
+
+void enemy::diagonal1move(){ 
+
+		if(mvwinch(curwin, yloc, xloc-1)!=32 || mvwinch(curwin, yloc-1, xloc)!=32){j=0;}
+		if(mvwinch(curwin, yloc, xloc+1)!=32 || mvwinch(curwin, yloc+1, xloc)!=32){j=1;}
+		mvwaddch(curwin, yloc, xloc, ' ');
+		if(j==0){
+		xloc++;
+		napms(50);
+		yloc++;
+		napms(50);
+		display();
+		}
+		if(j==1){
+		xloc--;
+		napms(50);
+		yloc--;
+		napms(50);
+		display();
+
+		}
+
+}
+
+
+
+void enemy::diagonal2move(){
+
+		
+		if(mvwinch(curwin, yloc, xloc+1)!=32 || mvwinch(curwin, yloc-1, xloc)!=32){j=0;}
+		if(mvwinch(curwin, yloc, xloc-1)!=32 || mvwinch(curwin, yloc+1, xloc)!=32){j=1;}
+		mvwaddch(curwin, yloc , xloc, ' ');
+		if(j==0){
+		xloc--;
+		napms(50);
+		yloc++;
+		napms(50);
+		display();
+		}
+		if(j==1){
+		xloc++;
+		napms(50);
+		yloc--;
+		napms(50);
+		display();
+
+		}
+
+}
+
+
+
+
+//void enemy::declife(int& dannoricevuto){vita=vita-dannoricevuto;}
+//bool enemy::checklife(){if(vita>0){return(true);} else{return(false);} }
+
+
+
 
 int enemy::getenemx(){return (xloc);}
 int enemy::getenemy(){return (yloc);}
 
 void enemy::undisplay(){mvwaddch(curwin, yloc, xloc , ' ' );}
+
+
+
+
+
+class shootingenemy : public enemy {
+	protected :
+		int xmax, ymax, xmin , ymin ;
+		int yloc_bullet [50];
+		int xloc_bullet [50];
+		bool maxbullets ; 
+		int k;
+		int kk;
+		int timer; 
+		int firerate;
+	public:
+		shootingenemy(WINDOW *win, int y, int x, char c, int xmax, int ymax, int xmin , int ymin, int firerate ) : enemy(win,y,x,c){
+				this->xmax= xmax;
+			this->ymax=ymax;
+			this->xmin= xmin;
+			this->ymin=ymin;
+			maxbullets= false;
+			xloc_bullet[0]= xloc+1;
+			yloc_bullet[0]= yloc; 
+			k=1;
+			kk=0;
+			this->firerate= firerate;
+			timer=0;
+		}
+		void rightenemsh();
+		void leftenemsh();
+		void upenemsh();
+		void downenemsh();
+
+
+};
+
+
+
+
+void shootingenemy::rightenemsh(){
+
+			
+		if(yloc==ymin){j=0;}
+		if(yloc==ymax){j=1;}
+		if(j==0){
+		mvdown();
+		display();
+		}
+		if(j==1){
+		mvup();
+		display();
+		}
+		if (timer==firerate){
+			
+				xloc_bullet[kk]= xloc+1;
+				yloc_bullet[kk]= yloc;}
+		if(maxbullets==false){k++;}
+		kk++;
+		timer++;
+		for(int i = 0 ; i<k ; i++){
+			if(yloc_bullet[i]!=0){
+				if(mvwinch(curwin, yloc_bullet[i], xloc_bullet[i])!=124){
+					mvwaddch(curwin , yloc_bullet[i], xloc_bullet[i], ' ' );}
+				xloc_bullet[i]++;
+				if(mvwinch(curwin, yloc_bullet[i], xloc_bullet[i])!=124){
+					mvwaddch(curwin , yloc_bullet[i], xloc_bullet[i], '*' );}}
+			if(mvwinch(curwin, yloc_bullet[i], xloc_bullet[i])==124)
+				{maxbullets=true; 
+				yloc_bullet[i]=0; 
+				xloc_bullet[i]=0; }
+			if(maxbullets==true && kk==k){kk=0;}
+		
+			if(timer==firerate+1){timer=0;}
+		}
+}
+
+
+
+void shootingenemy::upenemsh(){
+
+			
+		if(xloc==xmin){j=0;}
+		if(xloc==xmax){j=1;}
+		if(j==0){
+		mvright();
+		display();
+		}
+		if(j==1){
+		mvleft();
+		display();
+		}
+		if (timer==firerate){
+				xloc_bullet[kk]= xloc;
+				yloc_bullet[kk]= yloc-1;}
+		if(maxbullets==false){k++;}
+		kk++;
+		timer++;
+		for(int i = 0 ; i<k ; i++){
+			//if(yloc_bullet[i]!=0){
+				if(mvwinch(curwin, yloc_bullet[i], xloc_bullet[i])!=95){
+					mvwaddch(curwin , yloc_bullet[i], xloc_bullet[i], ' ' );}
+				yloc_bullet[i]--;
+				if(mvwinch(curwin, yloc_bullet[i], xloc_bullet[i])!=95){
+					mvwaddch(curwin , yloc_bullet[i], xloc_bullet[i], '*' );}//}
+			if(mvwinch(curwin, yloc_bullet[i], xloc_bullet[i])==95)
+				{maxbullets=true; 
+				yloc_bullet[i]=0; 
+				xloc_bullet[i]=0; }
+			if(maxbullets==true && kk==k){kk=0;}
+		
+			if(timer==firerate+1){timer=0;}
+		}
+}
+
+
+
+void shootingenemy::leftenemsh(){
+
+			
+		if(yloc==ymin){j=0;}
+		if(yloc==ymax){j=1;}
+		if(j==0){
+		mvdown();
+		display();
+		}
+		if(j==1){
+		mvup();
+		display();
+		}
+		if (timer==firerate){
+				xloc_bullet[kk]= xloc-1;
+				yloc_bullet[kk]= yloc;}
+		if(maxbullets==false){k++;}
+		kk++;
+		timer++;
+		for(int i = 0 ; i<k ; i++){
+			if(yloc_bullet[i]!=0){
+				if(mvwinch(curwin, yloc_bullet[i], xloc_bullet[i])!=124){
+					mvwaddch(curwin , yloc_bullet[i], xloc_bullet[i], ' ' );}
+				xloc_bullet[i]--;
+				if(mvwinch(curwin, yloc_bullet[i], xloc_bullet[i])!=124){
+					mvwaddch(curwin , yloc_bullet[i], xloc_bullet[i], '*' );}}
+			if(mvwinch(curwin, yloc_bullet[i], xloc_bullet[i])==124)
+				{maxbullets=true; 
+				yloc_bullet[i]=0; 
+				xloc_bullet[i]=0; }
+			if(maxbullets==true && kk==k){kk=0;}
+		
+			if(timer==firerate+1){timer=0;}
+		}
+}
+
+
+void shootingenemy::downenemsh(){
+
+			
+		if(xloc==xmin){j=0;}
+		if(xloc==xmax){j=1;}
+		if(j==0){
+		mvright();
+		display();
+		}
+		if(j==1){
+		mvleft();
+		display();
+		}
+		if (timer==firerate){
+				xloc_bullet[kk]= xloc;
+				yloc_bullet[kk]= yloc+1;}
+		if(maxbullets==false){k++;}
+		kk++;
+		timer++;
+		for(int i = 0 ; i<k ; i++){
+			if(xloc_bullet[i]!=0){
+				if(mvwinch(curwin, yloc_bullet[i], xloc_bullet[i])!=95){
+					mvwaddch(curwin , yloc_bullet[i], xloc_bullet[i], ' ' );}
+				yloc_bullet[i]++;
+				if(mvwinch(curwin, yloc_bullet[i], xloc_bullet[i])!=95){
+					mvwaddch(curwin , yloc_bullet[i], xloc_bullet[i], '*' );}}
+			if(mvwinch(curwin, yloc_bullet[i], xloc_bullet[i])==95)
+				{maxbullets=true; 
+				yloc_bullet[i]=0;
+				xloc_bullet[i]=0;
+				 }
+			if(maxbullets==true && kk==k){kk=0;}
+		
+			if(timer==firerate+1){timer=0;}
+		}
+}
+
+
 
 #endif
 
