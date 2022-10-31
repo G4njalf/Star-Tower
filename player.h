a95092f844b2c29be332c37abcacc56bd1c35d4d
KyoZD <102605858+DhahriAhmed@users.noreply.github.com>
2022-10-31 17:01:34 +0100

    Add files via upload

--- player.h
+++ player.h
@@ -1,28 +1,53 @@
 #ifndef _PLAYER_H_
 #define _PLAYER_H_
 
+
+struct personaggio{
+    int bx;
+    int by;
+    int id;
+    personaggio *next;
+};
+
+typedef personaggio *lpersonaggio;
+
+
+
+
+
+
+
+
 class Player
 {
   public :
-     Player(WINDOW *win, int y, int x, char c);
+     Player(WINDOW *win, int y, int x, char c, int firerate);
 
       void mvup();
       void mvdown();
       void mvleft();
       void mvright();
       int  getmv();
-
+      void addbullet();
+      void remove_bullet();
+      void addblank();
+      void remove_blank();
+      void stampa_bullets();
       int getplx();
       int getply();
       void display();
 
-      private:
-         int xLoc, yLoc, xMax, yMax;
+      protected:
+         int xLoc, yLoc, xMax, yMax, xb, yb, identity, counter, firerate;
+         bool flag;
          char character;
          WINDOW * curwin;
+         lpersonaggio  p ;
+         lpersonaggio  b ; 
+
 };
 
-Player::Player(WINDOW * win, int y, int x, char c)
+Player::Player(WINDOW * win, int y, int x, char c, int firerate)
 {
    curwin = win;
    yLoc = y;
@@ -30,6 +55,11 @@ Player::Player(WINDOW * win, int y, int x, char c)
    getmaxyx(curwin, yMax, xMax);
    keypad(curwin, true);
    character = c;
+   p = NULL;
+   this->firerate = firerate;
+   counter= firerate;
+   flag = true;  
+
 }
 
 
@@ -81,27 +111,88 @@ int Player::getmv()
   switch(choice)
     {
       case KEY_UP:
-        mvup();
+     if(mvwinch(curwin, yLoc-1 , xLoc)!=124 && mvwinch(curwin, yLoc-1 , xLoc)!=95){
+        mvup();}
 	wrefresh(curwin);
          break;
       case KEY_DOWN:
-        mvdown();
+      if(mvwinch(curwin, yLoc+1 , xLoc)!=124 && mvwinch(curwin, yLoc+1 , xLoc)!=95){
+        mvdown();}
 	wrefresh(curwin);
          break;
       case KEY_LEFT:
-        mvleft();
+      if(mvwinch(curwin, yLoc , xLoc-1)!=124 && mvwinch(curwin, yLoc , xLoc-1)!=95){
+        mvleft();}
 	wrefresh(curwin);
          break;
       case KEY_RIGHT:
-        mvright();
+      if(mvwinch(curwin, yLoc , xLoc+1)!=124 && mvwinch(curwin, yLoc , xLoc+1)!=95){
+        mvright();}
 	wrefresh(curwin);
          break;
-       default:
-         break;
+      case 119:
+        mvwaddch(curwin, yLoc, xLoc, character);
+        if(counter==firerate){
+         xb = xLoc;
+         yb = yLoc-1;
+         identity =  1 ;
+         if(mvwinch(curwin, yb , xb)!=124 && mvwinch(curwin, yb , xb)!=95){
+          addbullet();
+          addblank();}
+        flag=true;
+        }
+        break;
+      case 97:
+        mvwaddch(curwin, yLoc, xLoc, character);
+        if(counter==firerate){
+         xb = xLoc-1;
+         yb = yLoc;
+         identity =  2 ;
+         if(mvwinch(curwin, yb , xb)!=124 && mvwinch(curwin, yb , xb)!=95){
+          addbullet();
+          addblank();}
+        flag=true;
+        }
+        break;
+      case 115:
+        mvwaddch(curwin, yLoc, xLoc, character);
+        if(counter==firerate){
+         xb = xLoc;
+         yb = yLoc+1;
+         identity =  3 ;
+         if(mvwinch(curwin, yb , xb)!=124 && mvwinch(curwin, yb , xb)!=95){
+          addbullet();
+          addblank();}
+        flag=true;
+        }
+        break;
+      case 100:
+        mvwaddch(curwin, yLoc, xLoc, character);
+        if(counter==firerate){
+         xb = xLoc+1;
+         yb = yLoc;
+         identity =  4 ;
+         if(mvwinch(curwin, yb , xb)!=124 && mvwinch(curwin, yb , xb)!=95){
+          addbullet();
+          addblank();}
+        flag=true;
+        }
+        break;
+      default:
+      break;
  }
-  return choice;
-}
+    stampa_bullets();
+    if(counter!=firerate){counter++;}
+    if(counter==firerate && flag==true){
+      counter=0; 
+    }
+    flag=false;
 
+     
+    return choice;
+
+
+}
 
 
 
@@ -113,31 +204,173 @@ void Player::display()
 }
 int  Player::getplx(){return (xLoc);}
 int  Player::getply(){return (yLoc);}
+
+
+void Player:: addbullet(){
+	lpersonaggio tmp = p;
+	if(p==NULL){
+    if(mvwinch(curwin, yb , xb)!=124 ||mvwinch(curwin, yb , xb)!=95){
+		p = new personaggio;
+		p->bx=xb;
+		p->by=yb;
+		p->id = identity;
+		p->next = NULL;
+    }
+	}
+	else{
+		while(tmp->next!=NULL){
+			tmp = tmp->next;
+		}
+    if(mvwinch(curwin, yb , xb)!=124 ||mvwinch(curwin, yb , xb)!=95){
+		tmp->next = new personaggio;
+		tmp = tmp->next;
+		tmp->bx=xb;
+		tmp->by=yb;
+		tmp->id = identity;
+		tmp->next = NULL;
+    }
+	}
+	
+}
+
+void Player:: remove_bullet(){
+	lpersonaggio aux = p;
+	lpersonaggio prec = NULL;
+	lpersonaggio tmp;
+  int bbx, bby;
+	while(aux!=NULL){
+    bby = aux->by;
+    bbx = aux->bx;
+		if(mvwinch(curwin, bby , bbx)==124 ||mvwinch(curwin, bby , bbx)==95){
+			tmp = aux;
+			if(prec == NULL){
+				p = p->next ;
+			}else {
+				prec->next = aux->next ;
+			}
+			aux = aux->next;
+			delete (tmp);
+		}
+		else{
+			prec = aux ;
+			aux = aux->next ;
+		}
+	}
+	
+}
+
+
+
+
+
+
+void Player:: stampa_bullets(){
+    lpersonaggio tmp = p;
+	while (tmp!=NULL){    
+        if(tmp->id == 1 ){
+                mvwaddch(curwin, tmp->by, tmp->bx, '.');
+                tmp->by--;
+            }
+       else if(tmp->id == 2 ){
+                mvwaddch(curwin, tmp->by, tmp->bx, '.');
+                tmp->bx--;
+            }
+       else if(tmp->id == 3 ){
+                mvwaddch(curwin, tmp->by, tmp->bx, '.');
+                tmp->by++;
+            }
+       else if(tmp->id == 4 ){
+                mvwaddch(curwin, tmp->by, tmp->bx, '.');
+                tmp->bx++;
+            }    
+       tmp = tmp->next ;
+        }
+  remove_bullet();
+  tmp = b;
+while (tmp!=NULL){    
+        if(tmp->id == 1 ){
+          if(tmp->by!=yLoc || tmp->bx!=xLoc){
+                mvwaddch(curwin, tmp->by, tmp->bx, ' ');
+          }
+                tmp->by--;
+            }
+       else if(tmp->id == 2 ){
+        if(tmp->by!=yLoc || tmp->bx!=xLoc){
+                mvwaddch(curwin, tmp->by, tmp->bx, ' ');
+        }
+                tmp->bx--;
+            }
+       else if(tmp->id == 3 ){
+        if(tmp->by!=yLoc || tmp->bx!=xLoc){
+                mvwaddch(curwin, tmp->by, tmp->bx, ' ');
+        }
+                tmp->by++;
+            }
+       else if(tmp->id == 4 ){
+        if(tmp->by!=yLoc || tmp->bx!=xLoc){
+                mvwaddch(curwin, tmp->by, tmp->bx, ' ');
+        }
+                tmp->bx++;
+            }    
+       tmp = tmp->next ;
+        }
+		remove_blank();
+	}
+
+
+
+void Player:: remove_blank(){
+	lpersonaggio aux = b;
+	lpersonaggio prec = NULL;
+	lpersonaggio tmp;
+  int bbx, bby;
+	while(aux!=NULL){
+    bby = aux->by;
+    bbx = aux->bx;
+		if(mvwinch(curwin, bby , bbx)==124 ||mvwinch(curwin, bby , bbx)==95){
+			tmp = aux;
+			if(prec == NULL){
+				b = b->next ;
+			}else {
+				prec->next = aux->next ;
+			}
+			aux = aux->next;
+			delete (tmp);
+		}
+		else{
+			prec = aux ;
+			aux = aux->next ;
+		}
+	}
+	
+}
+
+void Player:: addblank(){
+	lpersonaggio tmp = b;
+	if(b==NULL){
+    if(mvwinch(curwin, yb , xb)!=124 ||mvwinch(curwin, yb , xb)!=95){
+		b = new personaggio;
+		b->bx=xLoc;
+		b->by=yLoc;
+		b->id = identity;
+		b->next = NULL;
+    }
+	}
+	else{
+		while(tmp->next!=NULL){
+			tmp = tmp->next;
+		}
+    if(mvwinch(curwin, yb , xb)!=124 ||mvwinch(curwin, yb , xb)!=95){
+		tmp->next = new personaggio;
+		tmp = tmp->next;
+		tmp->bx=xLoc;
+		tmp->by=yLoc;
+		tmp->id = identity;
+		tmp->next = NULL;
+    }
+	}
+	
+}
+
+
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- project.cpp
+++ project.cpp
@@ -1,4 +1,4 @@
-///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <ncurses.h>
 #include <thread>
 #include <string>
@@ -19,46 +19,66 @@ curs_set(0);
 int yMax, xMax;
 getmaxyx(stdscr, yMax, xMax);
 
-WINDOW * playwin = newwin(20, 50, (yMax/2)-10, 10);
+WINDOW * playwin = newwin(25, 80, (yMax/2)-10, 10);
 box(playwin, 0, 0);
 refresh();
 wrefresh(playwin);
-Player * p = new Player(playwin, 1, 1, '$');
-enemy * e= new enemy(playwin, 1 , 1 , 7 , 'X');
+Player * p = new Player(playwin, 1, 4, '$', 10);
+shootingenemy * e= new shootingenemy(playwin, 18 , 69 , 'X',0, 18 , 0 , 3, 6);
+//enemy *e= new enemy(playwin, 1, 48 , 'X');	
 
 
-
-//int j= 0;
 nodelay(playwin, TRUE);
 wtimeout(playwin, 1);
 int Life =3;
 
-
-// mvwprintw(playwin, 0 ,25, "|");
- mvwprintw(playwin, 1 ,25,"|");
- mvwprintw(playwin, 2 ,25,"|");
- mvwprintw(playwin, 3 ,25,"|");
+ /*mvwprintw(playwin, 19 ,0, "__________________________________________________");
+ mvwprintw(playwin, 0 ,0, "__________________________________________________");
+ mvwprintw(playwin, 1 ,0,"|");
+ mvwprintw(playwin, 2 ,0,"|");
+ mvwprintw(playwin, 3 ,,"|");
  mvwprintw(playwin, 4 ,25,"|");
  mvwprintw(playwin, 5 ,25,"|");
  mvwprintw(playwin, 6 ,25,"|");
  mvwprintw(playwin, 7 ,25,"|");
  mvwprintw(playwin, 8 ,25,"|");
-mvwprintw(playwin, 9 ,25,"|");
+ mvwprintw(playwin, 9 ,25,"|");
  mvwprintw(playwin, 10 ,25,"|");
  mvwprintw(playwin, 11 ,25,"|");
  mvwprintw(playwin, 12 ,25,"|");
  mvwprintw(playwin, 13 ,25,"|");
  mvwprintw(playwin, 14 ,25,"|");
  mvwprintw(playwin, 15 ,25,"|");
-mvwprintw(playwin, 16 ,25,"|");
-mvwprintw(playwin, 17 ,25,"|");
-mvwprintw(playwin, 18 ,25,"|");
-mvwprintw(playwin, 19 ,25,"|");
-mvwprintw(playwin, 20 ,25,"|");
+ mvwprintw(playwin, 16 ,25,"|");
+ mvwprintw(playwin, 17 ,25,"|");
+ mvwprintw(playwin, 18 ,25,"|");
+ mvwprintw(playwin, 19 ,25,"|");
+ //mvwprintw(playwin, 20 ,25,"|")*/
 
 
 
-
+            mvwprintw(playwin,1 ,1,"__|    |______________________________________________________________________");
+            mvwprintw(playwin,2 ,1,"__|    |______________________________________________________________________");
+            mvwprintw(playwin,3 ,1,"__|    |______|                                                     |_________");
+            mvwprintw(playwin,4 ,1,"__|    |______|                                                     |_________");
+            mvwprintw(playwin,5 ,1,"__|    |______|     _________________                               |_________");
+            mvwprintw(playwin,6 ,1,"__|    |______|    |_________________|                              |_________");
+            mvwprintw(playwin,7 ,1,"__|                |_________________|                              |_________");
+            mvwprintw(playwin,8 ,1,"__|                |_________________|                              |_________");
+            mvwprintw(playwin,9 ,1,"__|________________|_________________|                              |_________");
+            mvwprintw(playwin,10,1,"_____________________________________|                                        ");
+            mvwprintw(playwin,11,1,"_____________________________________|                                        ");
+            mvwprintw(playwin,12,1,"_____________________________________|                                ________");
+            mvwprintw(playwin,13,1,"____|                                                                |________");
+            mvwprintw(playwin,14,1,"____|                                                                |________");
+            mvwprintw(playwin,15,1,"____|                                                                |________");
+            mvwprintw(playwin,16,1,"____|                                                                |________");
+            mvwprintw(playwin,17,1,"____|                                                                |________");
+            mvwprintw(playwin,18,1,"____|                                                                |________");
+            mvwprintw(playwin,19,1,"____________________      ___________________________________________|________");
+            mvwprintw(playwin,20,1,"____________________|    |____________________________________________________");
+            mvwprintw(playwin,21,1,"____________________|    |____________________________________________________");
+            mvwprintw(playwin,22,1,"____________________|    |____________________________________________________");
 
 
 
@@ -67,10 +87,10 @@ mvwprintw(playwin, 20 ,25,"|");
 	if(wgetch(playwin) == ERR){}
 	else{
 	p->getmv();}
-	e->move();
+	e->leftenemsh();
 	wrefresh(playwin);
-	if (e->getenemx()==p->getplx() && e->getenemy()==p->getplx()){Life--;}   //per il momento il player uccide il nemico
- 	napms(1);
+	if (e->getenemx()==p->getplx() && e->getenemy()==p->getply()){Life--;}   
+ 	//napms(1);
 	}while(p->getmv()!='x' && Life>0);
 
 	e->undisplay();
