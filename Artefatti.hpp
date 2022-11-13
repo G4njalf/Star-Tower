fa0466931ff1d02f3dea0f6a09ac2f44569d0c18
mattiamal <102604998+mattiamal@users.noreply.github.com>
2022-11-13 11:44:29 +0100

    Update Artefatti.hpp

--- Artefatti.hpp
+++ Artefatti.hpp
@@ -1,45 +1,48 @@
 #include <ncurses.h>
 
-class artefatto{
+class artifact {
+public:
+    artifact(int y, int x, char c);
+
+    int cy();
+    int cx();
+    void draw(WINDOW * window);
+    void drawhpup(WINDOW * window);
+    bool collected (int y, int x);
+    bool return_remove();
+
 protected:
     int posX, posY;
-public:
-    artefatto(int y, int x){
+    char character;
+    bool remove = false;
+};
+artifact :: artifact(int y, int x, char c){
         posY = y;
         posX = x;
+        character = c;
     }
-    int cy(){
+    int artifact :: cy(){
         return posY;
     }
-    int cx(){
+    int artifact :: cx(){
         return posX;
     }
-    void cancella(WINDOW * window){
-        mvwaddch(window, posY, posX, ' ');
-        wrefresh(window);
+    bool artifact :: collected (int y, int x){                   //controlla se e' sopra all'artefatto, se si dice al player
+        bool check=false;                                    //di aggiungere il bonus e cancella l'artefatto
+        if (posY==y && posX==x){
+            remove = true;
+            check=true;
+        }
+        return check;
     }
-};
-class hpup : public artefatto{
-private:
-    bool elimina=false;
-    int vitapersonaggio;
-public:
-    hpup (int y, int x, int vitapers) : artefatto (y, x){
-        vitapersonaggio = vitapers;
+    bool artifact :: return_remove(){
+        return remove;
     }
-    void disegna (WINDOW * win){
+    void artifact :: draw (WINDOW * win){
+        mvwaddch(win, posY, posX, character);
+    }
+    void artifact :: drawhpup (WINDOW * win){
         mvwaddch(win, posY, posX, ACS_DIAMOND);
     }
-    bool raccolto (int y, int x){                   //controlla se e' sopra all'artefatto, se si aggiunge il bonus.
-        bool controllo=false;
-        if (posY==y && posX==x){
-            flash();
-            elimina = true;
-            controllo=true;
-        }
-        return controllo;
-    }
-    bool return_elimina(){
-        return elimina;
-    }
-};
+
+
